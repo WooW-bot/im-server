f1b6bae65bde4873fcaf31ca13c730251cfbf640
XO6J12G <xo6j12g@XO6J12GdeMacBook-Pro.local>
2025-12-21 16:10:56 +0700

    update code

--- tcp/src/main/java/com/pd/im/tcp/utils/RedissonTest.java
+++ tcp/src/main/java/com/pd/im/tcp/utils/RedissonTest.java
@@ -1,8 +1,12 @@
 package com.pd.im.tcp.utils;
 
+import org.redisson.PubSubMessageListener;
 import org.redisson.Redisson;
 import org.redisson.api.RBucket;
+import org.redisson.api.RMap;
+import org.redisson.api.RTopic;
 import org.redisson.api.RedissonClient;
+import org.redisson.api.listener.MessageListener;
 import org.redisson.client.codec.StringCodec;
 import org.redisson.config.Config;
 
@@ -18,9 +22,22 @@ public class RedissonTest {
         config.setCodec(stringCodec);
         RedissonClient redissonClient = Redisson.create(config);
 
-        RBucket<Object> im = redissonClient.getBucket("im");
+/*        RBucket<Object> im = redissonClient.getBucket("im");
         System.out.println(im.get());
         im.set("im");
-        System.out.println(im.get());
+        System.out.println(im.get());*/
+
+/*        RMap<String, String> imMap = redissonClient.getMap("imMap");
+        String client = imMap.get("client");
+        System.out.println(client);
+        imMap.put("client", "webClient");
+        System.out.println(imMap.get("client"));*/
+
+        RTopic topic = redissonClient.getTopic("topic");
+        topic.addListener(String.class, (charSequence, s) -> System.out.println("client1收到消息：" + s));
+        RTopic topic2 = redissonClient.getTopic("topic");
+        topic2.addListener(String.class, (charSequence, s) -> System.out.println("client2收到消息：" + s));
+        RTopic topic3 = redissonClient.getTopic("topic");
+        topic3.publish("hello world");
     }
 }
